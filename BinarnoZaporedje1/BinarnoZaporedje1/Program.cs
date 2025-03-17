faa18694e5708a41983b2b5dbe10c8bbfcf1b18b
tejvix10 <tadej.vizin10@gmail.com>
2025-03-17 16:35:53 +0100

    SeminarskaNaloga

---
+++ BinarnoZaporedje1/BinarnoZaporedje1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+class ObdelovalecPodatkov
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Vnesite številke ločene z vejicami:"); 
+        
+        int[] zaporedjeStevil = Console.ReadLine() 
+            .Split(',', StringSplitOptions.RemoveEmptyEntries) 
+            .Select(x => int.Parse(x.Trim())) 
+            .ToArray(); 
+
+        Console.Write("Vnesite iskano število: "); 
+        int iskanoStevilo = int.Parse(Console.ReadLine()); 
+
+       
+        int prviIndex = IsciPrvi(zaporedjeStevil, iskanoStevilo); 
+        int zadnjiIndex = IsciZadnji(zaporedjeStevil, iskanoStevilo); 
+
+        
+        Console.WriteLine($"Število {iskanoStevilo} se pojavi v intervalu: [{prviIndex}, {zadnjiIndex}]"); 
+    }
+
+    public static int IsciPrvi(int[] podatki, int cilj) 
+    {
+        int spodnjaMeja = 0, zgornjaMeja = podatki.Length - 1; 
+        int rezultat = -1; 
+
+        while (spodnjaMeja <= zgornjaMeja) 
+        {
+            int sredina = (spodnjaMeja + zgornjaMeja) / 2; 
+
+            if (podatki[sredina] == cilj) 
+            {
+                rezultat = sredina;
+                zgornjaMeja = sredina - 1; 
+            }
+            else if (podatki[sredina] < cilj) 
+            {
+                spodnjaMeja = sredina + 1; 
+            }
+            else
+            {
+                zgornjaMeja = sredina - 1; 
+            }
+        }
+
+        return rezultat; 
+    }
+
+    public static int IsciZadnji(int[] podatki, int cilj) 
+    {
+        int spodnjaMeja = 0, zgornjaMeja = podatki.Length - 1;
+        int rezultat = -1; 
+
+        while (spodnjaMeja <= zgornjaMeja) 
+        {
+            int sredina = (spodnjaMeja + zgornjaMeja) / 2; 
+
+            if (podatki[sredina] == cilj) 
+            {
+                rezultat = sredina; 
+                spodnjaMeja = sredina + 1;
+            }
+            else if (podatki[sredina] < cilj) 
+            {
+                spodnjaMeja = sredina + 1;
+            }
+            else
+            {
+                zgornjaMeja = sredina - 1; 
+            }
+        }
+
+        return rezultat; 
+    }
+}
---
+++ BinarnoZaporedje2/BinarnoZaporedje2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class OptimizatorPrenosa
+{
+    public int IzracunajMinimalnoKapaciteto(int[] mase, int steviloDni) 
+    {                                                                  
+                                                                        
+        int spodnjaMeja = 0, zgornjaMeja = 0; 
+
+        for (int i = 0; i < mase.Length; i++) 
+        {
+            spodnjaMeja = Math.Max(spodnjaMeja, mase[i]); 
+            zgornjaMeja += mase[i]; 
+        }
+
+       
+        while (spodnjaMeja < zgornjaMeja) 
+        {
+            int srednjaVrednost = (spodnjaMeja + zgornjaMeja) / 2; 
+            if (JeMoznoPrenesti(mase, steviloDni, srednjaVrednost)) zgornjaMeja = srednjaVrednost;
+            else spodnjaMeja = srednjaVrednost + 1; 
+                                                    
+        }
+        return spodnjaMeja; 
+    }
+
+    private bool JeMoznoPrenesti(int[] mase, int steviloDni, int kapaciteta) 
+    {
+        int dneviUporabljeni = 1, trenutnaMasa = 0; 
+                                                    
+
+        for (int i = 0; i < mase.Length; i++)  
+        {
+
+            if (trenutnaMasa + mase[i] > kapaciteta)
+            {
+                dneviUporabljeni++;     
+                trenutnaMasa = 0;     
+            }
+            trenutnaMasa += mase[i];  
+        }
+        return dneviUporabljeni <= steviloDni; 
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Vnesite število paketov:");
+        int stPaketov = int.Parse(Console.ReadLine()); 
+        int[] masePaketov = new int[stPaketov]; 
+
+        Console.WriteLine("Vnesite mase paketov (ločene z vejicami):"); 
+        string[] vhod = Console.ReadLine().Split(','); 
+        for (int i = 0; i < stPaketov; i++)
+        {
+            masePaketov[i] = int.Parse(vhod[i]);
+        }
+
+        Console.WriteLine("Vnesite število dni:"); 
+        int steviloDni = int.Parse(Console.ReadLine()); 
+
+        
+        Console.WriteLine("Optimalna minimalna kapaciteta: " + new OptimizatorPrenosa().IzracunajMinimalnoKapaciteto(masePaketov, steviloDni)); 
+    }
+}
